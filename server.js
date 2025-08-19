d9407bed738f82bae3ad6b9f30dc14165caa3d22
azuh salvation <salvationazuh@gmail.com>
2025-08-19 07:44:08 +0100

    update

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5500",
-      "https://eventhub-eight-gamma.vercel.app/",
+      "https://eventhub-eight-gamma.vercel.app",
     ],
     credentials: true,
   })
